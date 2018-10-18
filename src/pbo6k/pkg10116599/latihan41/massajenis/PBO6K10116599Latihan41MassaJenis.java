2f4808b81d93a93c28f108e9d249b0444b33731d
Roby <robyeka@email.unikom.ac.id>
2018-10-18 10:21:20 +0700

    ngoding

--- src/pbo6k/pkg10116599/latihan41/massajenis/PBO6K10116599Latihan41MassaJenis.java
+++ src/pbo6k/pkg10116599/latihan41/massajenis/PBO6K10116599Latihan41MassaJenis.java
@@ -7,7 +7,13 @@ package pbo6k.pkg10116599.latihan41.massajenis;
 
 /**
  *
- * @author eka
+ * @author 
+ * NAMA     : Robby Eka Purnama
+ * KELAS    : PBO6K
+ * NIM      : 10116599
+ * Deskripsi Program : Program untuk menghitung volume dan massa jenis kubus,
+ *                      dengan memasukkan nilai pada sisi dan massanya.
+ * 
  */
 public class PBO6K10116599Latihan41MassaJenis {
 
@@ -16,6 +22,23 @@ public class PBO6K10116599Latihan41MassaJenis {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        // Inisialisasi kubus
+        Kubus okubus = new Kubus();
+        okubus.setSisi(5);
+        okubus.setMassa(250);
+        
+        // Input nilai sisi dan masssa kubus
+        System.out.println("====== Massa Jenis Kubus ======");
+        System.out.println("Sisi\t: " +okubus.getSisi());
+        System.out.println("Massa\t: " +okubus.getMassa());
+        System.out.println();
+        
+        // Output hasil perhitungan
+        System.out.println("====== Hasil Perhitungan ======");
+        System.out.println("Volume\t\t: " +okubus.hitungVolume(okubus.getSisi()));
+        System.out.println("Massa Jenis\t: " +okubus.hitungMassaJenis(okubus.getMassa(), okubus.hitungVolume(0)));
+        
     }
     
 }
